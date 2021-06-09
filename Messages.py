fffcfc71f7cd35fe36a6938e2920509796fcedbd
Kwanele <kwanelegamedze4@gmail.com>
2021-06-09 14:50:08 +0200

    Made syntex more clean by adding spaces

--- Messages.py
+++ Messages.py
@@ -2,10 +2,15 @@ import random
 
 # user possible messages
 greeting = ["hi", "hello", "hey", "how are you", "sup"]
+
 ask = ["who are you", "who is this", "you are?", "who are you?", "who is thi?"]
+
 yeses = ["yes", "yep", "yeah", "yebo", "sur", "sure"]
+
 nos = ["nop", "no"]
+
 thank_you = ["Thanks", "thank you"]
+
 motivation = ["Hard work pays", "Don’t tell people your plans. Show them your results", "No pressure, no diamonds.",
               "When nothing goes right, go left.", "Try Again. Fail again. Fail better.",
               "Impossible is for the unwilling.",
